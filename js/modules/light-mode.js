659030b18a18e30b91c6e5205050d155af00d7f7
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2024-11-19 22:59:55 +0100

    btn light

--- js/modules/light-mode.js
+++ js/modules/light-mode.js
@@ -1,23 +1,8 @@
 export default function lightMode(params) {
+    const btn = document.querySelector('#light-mode')
+    btn.addEventListener('click', (params) => {
+        console.log('deu bom')
+    })
 
 }
 
-const lightButton = document.querySelector('#light-mode')
-
-lightButton.addEventListener('click', turnLightMode)
-
-function turnLightMode(params) {
-    if (!lightButton) {
-        console.error("Botão de modo claro não encontrado!");
-        return;
-    }
-    console.log('cliquei')
-    // const root = document.documentElement
-
-    // if (root.getPropertyValue('--background-white') === '#FFFFFF') {
-    //     root.style.setProperty('--background-white', 'black')
-    // } else {
-    //     root.style.setProperty('--background-white', '#FFFFFF')
-    // }
-
-}
